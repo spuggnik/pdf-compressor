7a2800567bf17b5fbd5a7f84fc2913bfe82d3767
spuggnik <niklaslorenz977@gmail.com>
2025-06-02 20:22:18 +0200

    modified:   src/components/NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 export default function NavBar() {
     return (
         <nav className="flex gap-4 p-4 bg-white/20 backdrop-blur border-b dark:bg-slate-800/80 border-b border-slate-200 dark:border-slate-700">
