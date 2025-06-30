8c0b34649f1a43e761c1c4defbec51528017802e
spuggnik <niklaslorenz977@gmail.com>
2025-06-30 19:09:17 +0200

    added type Props

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,26 +1,23 @@
+import { ReactNode } from "react";
 import NavBar from '@/components/NavBar';
 import { NextIntlClientProvider } from 'next-intl';
 
-export default async function LocaleLayout({
-  children,
-  params
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
-   const locale = params?.locale ?? 'en';
+type Props = {
+  children: ReactNode;
+  params: { locale: string};
+};
 
+export default async function LocaleLayout({ children, params }: Props) {
+  const awaitedParams = await params;
+  const locale = awaitedParams.locale ?? "en";
   const messages = await import(`../../../messages/${locale}.json`).then(m => m.default);
 
   return (
-    <html lang={locale}>
-      <body>
+    
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NavBar />
           {children}
         </NextIntlClientProvider>
-      </body>
-    </html>
   );
 }
 
