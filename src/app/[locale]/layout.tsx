3e3a3fe81e8e43105406fbd478275b538f21807d
spuggnik <niklaslorenz977@gmail.com>
2025-07-01 19:03:25 +0200

    removed await and using the params object directly

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,8 +2,7 @@ import NavBar from '@/components/NavBar';
 import { NextIntlClientProvider } from 'next-intl';
 
 export default async function LocaleLayout({ children, params }: { children: React.ReactNode; params: { locale: string } }) {
-  const awaitedParams = await params;
-  const locale = awaitedParams.locale ?? "en";
+  const locale = params.locale ?? "en";
   const messages = await import(`../../../messages/${locale}.json`).then(m => m.default);
 
   return (
