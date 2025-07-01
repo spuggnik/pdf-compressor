c1fc83589838be7707d259b63221276637d3bcb3
spuggnik <niklaslorenz977@gmail.com>
2025-07-01 18:57:48 +0200

    removed explicit proptypes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,15 @@
 import NavBar from '@/components/NavBar';
 import { NextIntlClientProvider } from 'next-intl';
-import type { LayoutProps } from 'next';
 
-export default async function LocaleLayout({ children, params }: LayoutProps<{ locale: string }>) {
+export default async function LocaleLayout({ children, params }: { children: React.ReactNode; params: any }) {
   const awaitedParams = await params;
   const locale = awaitedParams.locale ?? "en";
   const messages = await import(`../../../messages/${locale}.json`).then(m => m.default);
 
   return (
-    
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <NavBar />
-          {children}
-        </NextIntlClientProvider>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <NavBar />
+      {children}
+    </NextIntlClientProvider>
   );
 }
-
